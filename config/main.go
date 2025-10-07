75c7a90e47fdc37ebcf927a371732d6ce84240a1
AndrewLondon <andrewmart@mail.com>
2025-10-07 23:51:44 +0100

    Updated

--- config/main.go
+++ config/main.go
@@ -2,3 +2,4 @@ package config
 
 // AppConfig holds the application configuration settings.
 // I need to add more details here later.
+// Create the app web + app + api + db + cache + message broker
